76805f81fdedb1b39e457dce55abee5550eabd8c
sakib07 <csesakib07@gmail.com>
2019-07-11 18:26:27 +0600

    add category

--- routes/web.php
+++ routes/web.php
@@ -24,3 +24,10 @@ Route::get('/logout','SuperAdminController@logout');
 Route::get('/admin', 'AdminController@index');
 Route::get('/dashboard', 'AdminController@show_dashboard');
 Route::post('/admin-dashboard', 'AdminController@dashboard');
+
+//category related Route...............
+
+Route::get('/add-category','CategoryController@index');
+Route::get('/all-category','CategoryController@all_category');
+Route::post('/save-category','CategoryController@save_category');
+
